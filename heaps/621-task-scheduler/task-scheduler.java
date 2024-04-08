3c1f584df13e0f118ccf1659c4285fb443d740a0
Swapnil Singh <swappp7singh@gmail.com>
2024-04-08 10:19:29 +0530

    Time: 32 ms (13.37%) | Memory: 45.5 MB (64.95%) - LeetSync

--- heaps/621-task-scheduler/task-scheduler.java
+++ heaps/621-task-scheduler/task-scheduler.java
@@ -5,28 +5,27 @@ class Solution {
             map.put(task, map.getOrDefault(task, 0) + 1);
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         maxHeap.addAll(map.values());
 
         int cycles = 0;
-        while (!maxHeap.isEmpty()) {
-            int coolDownPeriod = n + 1;
-            int taskCount = 0;
-            List<Integer> store = new ArrayList<>();
 
-            while(coolDownPeriod-- > 0 && !maxHeap.isEmpty()) {
+        while(!maxHeap.isEmpty()) {
+            List<Integer> store = new ArrayList<>();
+            int coolDown = n + 1;
+            int taskCount = 0;
+
+            while(coolDown-- > 0 && !maxHeap.isEmpty()) {
                 int currentFrequency = maxHeap.poll();
-                if (currentFrequency > 1) {
-                    store.add(currentFrequency - 1);
-                }
-                taskCount += 1;
+                if (currentFrequency > 1) store.add(currentFrequency - 1);
+                taskCount++;
             }
 
             store.forEach(maxHeap::offer);
 
             cycles += maxHeap.isEmpty() ? taskCount : n + 1;
         }
-        
+
         return cycles;
     }
 }
