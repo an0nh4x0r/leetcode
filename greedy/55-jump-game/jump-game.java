e3d83e36c22e51c7dee70da8650bcf82d33b3588
Swapnil Singh <swappp7singh@gmail.com>
2024-04-10 10:39:35 +0530

    Time: 2 ms (82.16%) | Memory: 45.7 MB (29.84%) - LeetSync

--- greedy/55-jump-game/jump-game.java
+++ greedy/55-jump-game/jump-game.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean canJump(int[] nums) {
         int goal = nums.length - 1;
 
-        for (int i = nums.length - 1; i >= 0; --i) {
+        for (int i = nums.length - 1; i >= 0; --i)  {
             if (i + nums[i] >= goal) {
                 goal = i;
             }
         }
-
-        return goal == 0;       
+        
+        return goal == 0;
     }
 }
