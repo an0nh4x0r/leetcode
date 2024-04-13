6640db855114fcbb560c6704db992cf6aa7fcf56
Swapnil Singh <swappp7singh@gmail.com>
2024-04-13 23:43:41 +0530

    Time: 10 ms (25.66%) | Memory: 42.7 MB (12.85%) - LeetSync

--- greedy/768-partition-labels/partition-labels.java
+++ greedy/768-partition-labels/partition-labels.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         Map<Character, Integer> map = new HashMap<>();
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < arr.length; ++i) {
-            map.put(arr[i], i);
+
+        for (int i = 0; i < s.length(); ++i) {
+            map.put(s.charAt(i), i);
         }
 
         int size = 0;
         int partitionEnd = 0;
         List<Integer> output = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             size++;
-            partitionEnd = Math.max(partitionEnd, map.get(arr[i]));
+            partitionEnd = Math.max(partitionEnd, map.get(s.charAt(i)));
             if (partitionEnd == i) {
                 output.add(size);
                 size = 0;
