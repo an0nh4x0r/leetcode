c2a0c11b9c2114bbbb736ba3afc439c9b1d39b0e
Swapnil Singh <swappp7singh@gmail.com>
2024-04-14 00:11:59 +0530

    Time: 28 ms (72.71%) | Memory: 45.3 MB (65.90%) - LeetSync

--- greedy/876-hand-of-straights/hand-of-straights.java
+++ greedy/876-hand-of-straights/hand-of-straights.java
@@ -1,49 +1,24 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         Map<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i: hand) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        Arrays.sort(hand);
 
         for (int i = 0; i < hand.length; ++i) {
-            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
-        }
-
-        minHeap.addAll(map.keySet());
-        boolean isConsecutive = true;
-
-        while (!minHeap.isEmpty()) {
-            List<int[]> store = new ArrayList<>();
-            List<Integer> consecutive = new ArrayList<>();
-            
-            for (int i = 0; i < groupSize && !minHeap.isEmpty(); ++i) {
-                int currentNum = minHeap.poll();
-                int currentFreq = map.get(currentNum);
-                if (currentFreq > 1) store.add(new int[] {currentNum, currentFreq - 1});
-                consecutive.add(currentNum);
-            }
-
-            if (consecutive.size() < groupSize) {
-                isConsecutive = false;
-                break;
-            }
-
-            for (int i = 0; i < store.size(); ++i) {
-                int[] element = store.get(i);
-                minHeap.offer(element[0]);
-                map.put(element[0], element[1]);
-            }
-
-            int el = consecutive.get(0);
-            for (int i = 1; i < consecutive.size(); ++i) {
-                if (el + 1 != consecutive.get(i)) {
-                    isConsecutive = false;
-                    break;
+            int element = hand[i];
+            if (map.getOrDefault(element, 0) > 0) {
+                map.put(element, map.get(element) - 1);
+                for (int k = 1; k < groupSize; ++k) {
+                    element++;
+                    if (map.getOrDefault(element, 0) == 0) return false;
+                    else if (map.get(element) > 0) map.put(element, map.get(element) - 1);
                 }
-                el = consecutive.get(i);
             }
-
-            if (!isConsecutive) break;
         }
-
-        return isConsecutive;
+        
+        return true;
     }
 }
