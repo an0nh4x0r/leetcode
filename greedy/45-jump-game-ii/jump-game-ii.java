d5217bab8166aafbb8fa99b7b22cae469aabc9bf
Swapnil Singh <swappp7singh@gmail.com>
2024-04-14 00:24:57 +0530

    Time: 1 ms (99.09%) | Memory: 45.1 MB (51.88%) - LeetSync

--- greedy/45-jump-game-ii/jump-game-ii.java
+++ greedy/45-jump-game-ii/jump-game-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int jump(int[] nums) {
-        int maxJump = 0;
         int left = 0;
         int right = 0;
+        int maxJump = 0;
         int jump = 0;
 
         while (right < nums.length - 1) {
@@ -13,7 +13,6 @@ class Solution {
             right = maxJump;
             jump += 1;
         }
-
-        return jump;        
+        return jump;
     }
 }
