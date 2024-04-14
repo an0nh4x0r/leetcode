c572e684268a2aee586118311e7b2a01c3f313ec
Swapnil Singh <swappp7singh@gmail.com>
2024-04-14 09:18:56 +0530

    Time: 7 ms (32.66%) | Memory: 110 MB (79.24%) - LeetSync

--- greedy/2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ greedy/2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-        int[] response = new int[3];
+        int[] r = new int[3];
 
         for (var s: triplets) {
             if (s[0] <= target[0] && s[1] <= target[1] && s[2] <= target[2]) {
-                response = new int[] {Math.max(s[0], response[0]), Math.max(s[1], response[1]), Math.max(s[2], response[2])};
+                r = new int[] {Math.max(s[0], r[0]), Math.max(s[1], r[1]), Math.max(s[2], r[2])};
             }
         }
 
-        return Arrays.equals(target, response);        
+        return Arrays.equals(r, target);        
     }
 }
