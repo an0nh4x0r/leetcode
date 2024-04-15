48dc1e8ea078a3310a003dc5b50c55a9eaa70ef0
Swapnil Singh <swappp7singh@gmail.com>
2024-04-15 10:20:21 +0530

    Time: 1 ms (98.58%) | Memory: 45.1 MB (6.82%) - LeetSync

--- intervals/57-insert-interval/insert-interval.java
+++ intervals/57-insert-interval/insert-interval.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = new ArrayList<>();
+        
         for (int[] interval: intervals) {
             if (newInterval == null || interval[1] < newInterval[0]) {
                 res.add(interval);
