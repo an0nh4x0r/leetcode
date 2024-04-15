3836ab0a2ee7c077b0e368dbe6f95cb01ba5965a
Swapnil Singh <swappp7singh@gmail.com>
2024-04-15 18:47:07 +0530

    Time: 1 ms (98.58%) | Memory: 44.7 MB (69.72%) - LeetSync

--- intervals/57-insert-interval/insert-interval.java
+++ intervals/57-insert-interval/insert-interval.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = new ArrayList<>();
-        
+
         for (int[] interval: intervals) {
             if (newInterval == null || interval[1] < newInterval[0]) {
                 res.add(interval);
@@ -18,7 +18,7 @@ class Solution {
         if (newInterval != null) {
             res.add(newInterval);
         }
-
+        
         return res.toArray(new int[res.size()][]);
     }
 }
