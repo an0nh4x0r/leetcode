f3f1fa497141639380d6a7bf9cec725ef87734e9
Swapnil Singh <swappp7singh@gmail.com>
2024-04-21 18:56:14 +0530

    Time: 1 ms (98.62%) | Memory: 41.5 MB (29.39%) - LeetSync

--- stack/20-valid-parentheses/valid-parentheses.java
+++ stack/20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
         
+        Stack<Character> stack = new Stack<>();
+
         for (char c: s.toCharArray()) {
             if (c == '(') {
                 stack.push(')');
@@ -9,7 +10,7 @@ class Solution {
                 stack.push('}');
             } else if (c == '[') {
                 stack.push(']');
-            } else if (stack.isEmpty() || stack.pop() != c) {
+            } else if (stack.isEmpty() || c != stack.pop()) {
                 return false;
             }
         }
