a4101d094b9278d1d8844b5eb8cc77ba6569dd19
Swapnil Singh <swappp7singh@gmail.com>
2024-04-21 21:26:04 +0530

    Time: 4 ms (12.94%) | Memory: 44.3 MB (12.39%) - LeetSync

--- stack/22-generate-parentheses/generate-parentheses.java
+++ stack/22-generate-parentheses/generate-parentheses.java
@@ -1,31 +1,33 @@
 class Solution {
 
-    List<String> res = new ArrayList<>();
     Stack<Character> stack = new Stack<>();
+    List<String> res = new ArrayList<>();
 
     public List<String> generateParenthesis(int n) {
-        backTrack(0, 0, n);
+        backtrack(0, 0, n);        
         return res;
     }
 
-    private void backTrack(int open, int close, int n) {
-        if (open  == close && close == n) {
+    private void backtrack(int open, int close, int n) {
+        if (open == close && close == n) {
             StringBuilder sb = new StringBuilder();
+
             for (Character c: stack) {
                 sb.append(c);
             }
+
             res.add(sb.toString());
         }
 
         if (open < n) {
             stack.push('(');
-            backTrack(open + 1, close, n);
+            backtrack(open + 1, close, n);
             stack.pop();
         }
 
         if (close < open) {
             stack.push(')');
-            backTrack(open, close + 1, n);
+            backtrack(open, close + 1, n);
             stack.pop();
         }
     }
