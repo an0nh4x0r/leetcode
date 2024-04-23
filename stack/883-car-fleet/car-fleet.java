7effae64e42deefdaaa449e9beaee9f19f25a189
Swapnil Singh <swappp7singh@gmail.com>
2024-04-23 11:29:29 +0530

    Time: 101 ms (17.26%) | Memory: 61.7 MB (9.45%) - LeetSync

--- stack/883-car-fleet/car-fleet.java
+++ stack/883-car-fleet/car-fleet.java
@@ -1,27 +1,20 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        TreeMap<Integer, Double> map = new TreeMap<>(Collections.reverseOrder());
+        TreeMap<Integer, Double> map = new TreeMap<>(Comparator.reverseOrder());
+        int fleet = 0;
 
-        // DISTANCE = SPEED * TIME 
-        // TIME = DISTANCE / SPEED
-        // WE are getting the positions as key and time as value
-        // this treemap works stores the position in descending order
         for (int i = 0; i < position.length; ++i) {
             map.put(position[i], (double) (target - position[i]) / speed[i]);
         }
 
-        double currentTime = 0;
-        int result = 0;
-
-        // if a car is taking more time, then the previous car, in the treemap.
-        // it is considered a new car fleet.
+        double curTime = 0;
         for (double time: map.values()) {
-            if (time > currentTime) {
-                currentTime = time;
-                result++;
+            if (time > curTime) {
+                fleet++;
+                curTime = time;
             }
         }
-
-        return result;
+        
+        return fleet;
     }
 }
