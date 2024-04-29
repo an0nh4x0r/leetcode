df1a6a1f9ad4894df22369644321194bbb11ed9a
Swapnil Singh <swappp7singh@gmail.com>
2024-04-29 10:38:50 +0530

    Time: 5 ms (71.24%) | Memory: 58.1 MB (17.12%) - LeetSync

--- two-pointers/11-container-with-most-water/container-with-most-water.java
+++ two-pointers/11-container-with-most-water/container-with-most-water.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxArea(int[] height) {
-        int res = 0;
-        int left = 0; 
+        int maxArea = 0;
+        int left = 0;
         int right = height.length - 1;
 
         while (left < right) {
-            // (right - left) will give me width
-            // and we need to take the minimum height such that the water doesn't spill to get the actual height
-            int area = (right - left)* Math.min(height[left], height[right]);
-            res = Math.max(res, area);
+
+            int area = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, area);
 
             if (height[left] < height[right]) {
                 left++;
@@ -17,6 +16,6 @@ class Solution {
             }
         }
 
-        return res;
+        return maxArea;
     }
 }
