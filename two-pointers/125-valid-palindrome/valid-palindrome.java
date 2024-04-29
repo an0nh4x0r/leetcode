d8e9d0e4d9c74ac75dc1a66cc3c6b1f4c1f2294f
Swapnil Singh <swappp7singh@gmail.com>
2024-04-29 08:48:23 +0530

    Time: 2 ms (98.92%) | Memory: 43 MB (61.23%) - LeetSync

--- two-pointers/125-valid-palindrome/valid-palindrome.java
+++ two-pointers/125-valid-palindrome/valid-palindrome.java
@@ -4,18 +4,15 @@ class Solution {
         int right = s.length() - 1;
 
         while (left < right) {
-
             if (!Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             } else if (!Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
-            } else if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                return false;
             } else {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
                 left++;
                 right--;
             }
-
         }
 
         return true;
