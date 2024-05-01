e1874db465df499ed80e45550af7ace3777445c3
Swapnil Singh <swappp7singh@gmail.com>
2024-05-01 16:31:29 +0530

    Time: 12 ms (51.14%) | Memory: 58.1 MB (58.73%) - LeetSync

---
+++ two-pointers/217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n: nums) {
+            if (map.containsKey(n)) return true;
+            else map.put(n, 1);
+        }
+        return false;
+    }
+}
