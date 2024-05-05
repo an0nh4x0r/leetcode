3af60af28beceeef08eec13b776e3482a85f1a45
Swapnil Singh <swappp7singh@gmail.com>
2024-05-05 13:11:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (82.53%) - LeetSync

--- arrays_and_hashing/792-binary-search/binary-search.java
+++ arrays_and_hashing/792-binary-search/binary-search.java
@@ -1,6 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int res = Arrays.binarySearch(nums, target);
-        return res < 0 ? -1 : res;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
     }
 }
