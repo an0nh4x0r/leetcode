e9b0620fbeff193d6feef8d8ebfe5ba8d7f55653
Swapnil Singh <swappp7singh@gmail.com>
2024-05-06 22:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (58.78%) - LeetSync

--- binary-search/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ binary-search/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,31 +1,45 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0) return false;
-
+        // calculate the rows and columns
         int rows = matrix.length;
-        int columns = matrix[0].length;
+        int cols = matrix[0].length;
 
-        int low = 0;
-        int high = rows * columns - 1;
-        
+        // create the variable for midIndex, midElement
+        // rowIndex, colIndex
         int midIndex;
         int midElement;
         int rowIndex;
         int colIndex;
 
-        while (low <= high) {
-            midIndex = (low + high) >>> 1;
-            
-            rowIndex = midIndex / columns;
-            colIndex = midIndex % columns;
+        // initialize low and high variable
+        // set low to 0
+        // set high to ((rows * cols) - 1)
+        int low = 0;
+        int high = rows * cols - 1;
 
+        // for binary search this is the template
+        // low should be less than or equal to high
+        while (low <= high) {
+            // >>> this is unsigned right shift operator
+            // which makes sure to perform the division 
+            // This ensures that the division is performed 
+            // without considering the sign of the operands.
+            midIndex = (low + high) >>> 1;
+
+            // rowIndex = midIndex / cols
+            rowIndex = midIndex / cols;
+            // colIndex = midIndex % cols
+            colIndex = midIndex % cols;
+
+            // get the midElement using the rowIndex and colIndex
             midElement = matrix[rowIndex][colIndex];
 
+            // now perform the standard binary search template
             if (midElement > target) high = midIndex - 1;
-            else if(midElement < target) low = midIndex + 1;
-            else return true;
+            else if (midElement < target) low = midIndex + 1;
+            else return true; 
         }
 
-        return false;  
+        return false;
     }
 }
