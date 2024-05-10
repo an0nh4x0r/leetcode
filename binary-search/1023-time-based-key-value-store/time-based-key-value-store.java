5b3eaad20e48c4315172e4a0ff81e782726d4a38
Swapnil Singh <swappp7singh@gmail.com>
2024-05-10 09:19:31 +0530

    Time: 149 ms (51.12%) | Memory: 119.2 MB (66.79%) - LeetSync

--- binary-search/1023-time-based-key-value-store/time-based-key-value-store.java
+++ binary-search/1023-time-based-key-value-store/time-based-key-value-store.java
@@ -8,12 +8,12 @@ class TimeMap {
     
     public void set(String key, String value, int timestamp) {
         List<Pair<String, Integer>> list = map.getOrDefault(key, new ArrayList<>());
-        list.add(new Pair(value, timestamp));
+        list.add(new Pair<String, Integer>(value, timestamp));
         map.put(key, list);
     }
     
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
+        if (map.get(key) == null) return "";
 
         List<Pair<String, Integer>> list = map.get(key);
         
@@ -24,12 +24,8 @@ class TimeMap {
         while (low <= high) {
             int mid = (low + high) >>> 1;
 
-            if (list.get(mid).getValue() <= timestamp) {
-                index = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            if (list.get(mid).getValue() <= timestamp) {index = mid; low = mid + 1;}
+            else high = mid - 1;
         }
 
         return index != -1 ? list.get(index).getKey() : "";
