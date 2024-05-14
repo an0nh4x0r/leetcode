aba46cff0f90db774b79c41e77873d501350e727
Swapnil Singh <swappp7singh@gmail.com>
2024-05-15 01:18:49 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (31.66%) - LeetSync

--- binary-search/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ binary-search/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -5,25 +5,27 @@ class Solution {
 
         if (m > n) return findMedianSortedArrays(nums2, nums1);
 
-        int left = 0, right = m;
         int half = (m + n + 1) / 2;
 
-        while (left <= right) {
-            int partition1 = (left + right) >>> 1;
-            int partition2 = half - partition1;
+        int left = 0, right = m;
 
-            int left1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
-            int right1 = (partition1 == m) ? Integer.MAX_VALUE : nums1[partition1];
-            int left2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
-            int right2 = (partition2 == n) ? Integer.MAX_VALUE : nums2[partition2];
+        while (left <= right) {
+            int p1 = (left + right) >>> 1;
+            int p2 = half - p1;
+
+            int left1 = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1 - 1];
+            int right1 = (p1 == m) ? Integer.MAX_VALUE : nums1[p1];
+            int left2 = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2 - 1];
+            int right2 = (p2 == n) ? Integer.MAX_VALUE : nums2[p2];
 
             if (left1 <= right2 && left2 <= right1) {
-                if ((m+n) % 2 == 0) {
+                if ((m + n) % 2 == 0) {
                     return (Math.max(left1, left2) + Math.min(right1, right2)) / 2d;
-                } 
-                else return Math.max(left1, left2);
-            } else if (left1 > right2) right = partition1 - 1;
-            else left = partition1 + 1;
+                } else {
+                    return Math.max(left1, left2);
+                }
+            } else if (left1 > right2) right = p1 - 1;
+            else left = p1 + 1;
         }
 
         return 0.0;
