4a43859f5ad71fe4935e75b6e98636d827511950
Swapnil Singh <swappp7singh@gmail.com>
2024-05-16 21:46:47 +0530

    Time: 29 ms (12.20%) | Memory: 44.8 MB (82.57%) - LeetSync

--- binary-search/907-koko-eating-bananas/koko-eating-bananas.java
+++ binary-search/907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,26 +1,25 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-
-        int k = Arrays.stream(piles).max().getAsInt();
         int low = 1;
-        int high = k;
-        int result = k;
+        int high = Arrays.stream(piles).max().getAsInt();
+        int result = high;
 
         while (low <= high) {
             int mid = (low + high) >>> 1;
+
             int hours = 0;
-            for(int i = 0; i < piles.length; ++i) {
-                hours += Math.ceil((double) piles[i] / mid);
+            for (var pile: piles) {
+                hours += Math.ceil((double) pile / mid);
             }
 
             if (hours <= h) {
-                result = mid;
                 high = mid - 1;
-            } else low = mid + 1;
-
-            
+                result = Math.min(result, mid);
+            } else {
+                low = mid + 1;
+            }
         }
 
-        return result;        
+        return result;
     }
 }
