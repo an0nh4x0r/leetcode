bc643746d149659f8d723fe5f4cf1b55c524a31f
Swapnil Singh <swappp7singh@gmail.com>
2024-05-16 22:04:35 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (98.98%) - LeetSync

--- binary-search/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ binary-search/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,34 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
         int m = nums1.length;
         int n = nums2.length;
 
-        if (m > n)
-            return findMedianSortedArrays(nums2, nums1);
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
 
-        int left = 0, right = m, halfLength = (m + n + 1) / 2;
-        var result = 0.0;
+        int low = 0;
+        int high = m;
 
-        while (left <= right) {
-            int partitionA = (left + right) >>> 1;
+        int halfLength = (m + n + 1) / 2;
+        var result = 0d;
+
+        while (low <= high) {
+            int partitionA = (low + high) >>> 1;
             int partitionB = halfLength - partitionA;
 
-            int maxX = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1];
-            int minX = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA];
-            int maxY = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
-            int minY = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB];
+            int left1 = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1];
+            int right1 = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA];
+            int left2 = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
+            int right2 = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB];
 
-            if (maxX <= minY && maxY <= minX) {
+            if (left1 <= right2 && left2 <= right1) {
                 if ((m + n) % 2 == 0) {
-                    result = (Math.max(maxX, maxY) + Math.min(minY, minX)) / 2d;
+                    result = (Math.max(left1, left2) + Math.min(right1, right2)) / 2d;
                 } else {
-                    result = Math.max(maxX, maxY);
+                    result = Math.max(left1, left2);
                 }
                 break;
-            } else if (maxX > minY) {
-                right = partitionA - 1;
+            } else if (left1 > right2) {
+                high = partitionA - 1;
             } else {
-                left = partitionA + 1;
+                low = partitionA + 1;
             }
         }
 
