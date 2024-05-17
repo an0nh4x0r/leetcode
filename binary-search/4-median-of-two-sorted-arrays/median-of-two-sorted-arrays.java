a398ebe8ee3c261e3fee656d07e578d8fc887794
Swapnil Singh <swappp7singh@gmail.com>
2024-05-17 11:12:23 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (84.17%) - LeetSync

--- binary-search/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ binary-search/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,6 +1,5 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
         int m = nums1.length;
         int n = nums2.length;
 
@@ -8,18 +7,18 @@ class Solution {
 
         int low = 0;
         int high = m;
+        int half = (m + n + 1) / 2;
 
-        int halfLength = (m + n + 1) / 2;
-        var result = 0d;
+        double result = 0.0;
 
         while (low <= high) {
-            int partitionA = (low + high) >>> 1;
-            int partitionB = halfLength - partitionA;
+            int partA = (low + high) >>> 1;
+            int partB = half - partA;
 
-            int left1 = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1];
-            int right1 = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA];
-            int left2 = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
-            int right2 = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB];
+            int left1 = (partA == 0) ? Integer.MIN_VALUE : nums1[partA - 1];
+            int right1 = (partA == m) ? Integer.MAX_VALUE : nums1[partA];
+            int left2 = (partB == 0) ? Integer.MIN_VALUE : nums2[partB - 1];
+            int right2 = (partB == n) ? Integer.MAX_VALUE : nums2[partB];
 
             if (left1 <= right2 && left2 <= right1) {
                 if ((m + n) % 2 == 0) {
@@ -28,11 +27,8 @@ class Solution {
                     result = Math.max(left1, left2);
                 }
                 break;
-            } else if (left1 > right2) {
-                high = partitionA - 1;
-            } else {
-                low = partitionA + 1;
-            }
+            } else if (left1 > right2) high = partA - 1;
+            else low = partA + 1;
         }
 
         return result;
