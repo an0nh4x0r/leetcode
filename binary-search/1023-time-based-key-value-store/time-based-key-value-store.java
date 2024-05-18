002a41b8458b2dc5e138da1ffbbf87bbb4eaa7c9
Swapnil Singh <swappp7singh@gmail.com>
2024-05-18 19:45:54 +0530

    Time: 150 ms (48.78%) | Memory: 118.3 MB (81.21%) - LeetSync

--- binary-search/1023-time-based-key-value-store/time-based-key-value-store.java
+++ binary-search/1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,21 +1,23 @@
+record Pair(String key, int value) {}
+
 class TimeMap {
 
-    Map<String, List<Pair<String, Integer>>> map = new HashMap<>();
+    Map<String, List<Pair>> map = new HashMap<>();
 
     public TimeMap() {
         
     }
     
     public void set(String key, String value, int timestamp) {
-        List<Pair<String, Integer>> list = map.getOrDefault(key, new ArrayList<>());
-        list.add(new Pair<String, Integer>(value, timestamp));
+        List<Pair> list = map.getOrDefault(key, new ArrayList<>());
+        list.add(new Pair(value, timestamp));
         map.put(key, list);
     }
     
     public String get(String key, int timestamp) {
         if (map.get(key) == null) return "";
 
-        List<Pair<String, Integer>> list = map.get(key);
+        List<Pair> list = map.get(key);
         int low = 0;
         int high = list.size() - 1;
         int index = -1;
@@ -23,20 +25,21 @@ class TimeMap {
         while (low <= high) {
             int mid = (low + high) >>> 1;
 
-            if (list.get(mid).getValue() <= timestamp) {
+            if (list.get(mid).value() <= timestamp) {
                 index = mid;
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            else high = mid - 1;
         }
 
-        return index != -1 ? list.get(index).getKey() : "";
+        return index != -1 ? list.get(index).key() : "";
     }
 }
 
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
- * obj.set(key,value,timestamp);
- * String param_2 = obj.get(key,timestamp);
+ * obj.set(key, value, timestamp);
+ * String param_2 = obj.get(key, timestamp);
  */
