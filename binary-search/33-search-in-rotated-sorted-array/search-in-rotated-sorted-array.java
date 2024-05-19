d4640c9a3300df3c300b53a922904bd0c4220010
Swapnil Singh <swappp7singh@gmail.com>
2024-05-19 12:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (24.61%) - LeetSync

--- binary-search/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ binary-search/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,9 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int index = -1;
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] == target) index = i;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            int comparator = nums[mid];
+
+            if ((target < nums[0] && nums[mid] < nums[0]) || (target >= nums[0] && nums[mid] >= nums[0])) {
+                comparator = nums[mid];
+            } else {
+                if (target < nums[0]) {
+                    comparator = Integer.MIN_VALUE;
+                } else {
+                    comparator = Integer.MAX_VALUE;
+                }
+            }
+
+            if (comparator > target) right = mid - 1;
+            else if (comparator < target) left = mid + 1;
+            else return mid;
         }
-        return index;
+
+        return -1;
     }
 }
