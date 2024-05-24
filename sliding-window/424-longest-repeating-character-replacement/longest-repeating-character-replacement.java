fffa1919117f0d65b9d2275107abc1826666466a
Swapnil Singh <swappp7singh@gmail.com>
2024-05-24 22:31:29 +0530

    Time: 11 ms (51.63%) | Memory: 42.4 MB (70.53%) - LeetSync

--- sliding-window/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ sliding-window/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,30 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // english alphabets has 26 letters from A-Z
-        // this array will store all the upper case letters
-        int[] store = new int[26];
+        int[] freq = new int[26];
+        int mostFreqLetter = 0;
+        int left = 0;
+        int max = 0;
 
-        int result = 0; // final result
-        int maxFrequency = 0; // maximum frequency of a character 
-        
-        int left = 0; // left pointer of the sliding window
-        int right = 0; // right pointer of the sliding window
+        for (int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right) - 'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
 
-        while (right < s.length()) {
-            store[s.charAt(right) - 'A']++; // increment the frequency of the incoming letter
-            maxFrequency = Math.max(maxFrequency, store[s.charAt(right) - 'A']);
-            // right - left + 1 is used to calculate the size of the sliding window
+            int lettersToChange = (right - left + 1) - mostFreqLetter;
 
-            if ((right - left + 1) - maxFrequency > k) {
-                store[s.charAt(left) - 'A']--;
+            if (lettersToChange > k) {
+                freq[s.charAt(left) - 'A']--;
                 left++;
             }
 
-            // right - left + 1 is used to calculate the size of the sliding window
-            result = Math.max(result, (right - left + 1)); 
-            right++;
+            max = Math.max(max, right - left + 1);
         }
 
-        return result;
+        return max;
     }
 }
