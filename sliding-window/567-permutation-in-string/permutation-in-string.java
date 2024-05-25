3d6bdd176b48faa9738b4e8d65229f9005956ae9
Swapnil Singh <swappp7singh@gmail.com>
2024-05-25 21:25:12 +0530

    Time: 7 ms (72.67%) | Memory: 42.6 MB (57.30%) - LeetSync

--- sliding-window/567-permutation-in-string/permutation-in-string.java
+++ sliding-window/567-permutation-in-string/permutation-in-string.java
@@ -1,25 +1,40 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] s1Freq = new int[26];
-        int[] s2Freq = new int[26];
+        int[] freqS1 = new int[26]; // total frequency of s1
+        int[] freqS2 = new int[26]; // sliding window frequency on s2
 
-        for (char c1: s1.toCharArray()) s1Freq[c1 - 'a']++;
+        // populate the total frequency of s1 in freqS1
+        for (char c: s1.toCharArray()) {
+            freqS1[c - 'a'] += 1;
+        }
 
-        int left = 0;
-        int right = 0;
-        
-        while(right < s2.length()) {
-            s2Freq[s2.charAt(right) - 'a']++;
+        int left = 0; // start of sliding window
+        int right = 0; // end of sliding window
 
-            if ((right - left + 1) == s1.length()) {
-                if (Arrays.equals(s1Freq, s2Freq)) return true;
-            }
+        // standard sliding window template
+        while (right < s2.length()) {
 
+            // update the freqS2
+            freqS2[s2.charAt(right) - 'a'] += 1;
+
+            // check if the sliding window size is equal to the length of s1
+            // and validate if both the frequency arrays are equal
+            // return true
+            if ((right - left + 1) == s1.length() && Arrays.equals(freqS1, freqS2)) return true;
+
+            // otherwise check if the sliding window size is greater than or equal to the 
+            // s1 length size, in this case we need to move the left pointer
+            // note we are checking for the equals again here because, 
+            // if it is equal and matched we already would have return true in above condition
+            // since it is equal and not same, it means we need to move the left pointer in
+            // this case as well. 
             if ((right - left + 1) >= s1.length()) {
-                s2Freq[s2.charAt(left) - 'a']--;
+                freqS2[s2.charAt(left) - 'a'] -= 1;
                 left += 1;
             }
 
+            // move the right pointer of sliding window in all cases. 
+            // standard sliding window template
             right += 1;
         }
 
