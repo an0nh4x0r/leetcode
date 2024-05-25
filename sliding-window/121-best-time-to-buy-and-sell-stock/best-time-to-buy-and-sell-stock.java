3073dab121a05d82b3ed3e40d8ca7c4dcad5cfb0
Swapnil Singh <swappp7singh@gmail.com>
2024-05-25 21:48:22 +0530

    Time: 2 ms (76.59%) | Memory: 61.9 MB (14.48%) - LeetSync

--- sliding-window/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ sliding-window/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,32 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = 0;
-        int sell = 0;
-        int maxProfit = 0;
+        int left = 0; // start of the sliding window
+        int right = 0; // end of the sliding window
+        int res = 0; // final result
 
-        while (sell < prices.length) {
-            if (prices[buy] < prices[sell]) {
-                maxProfit = Math.max(maxProfit, prices[sell] - prices[buy]);
+        // standard sliding window template
+        while (right < prices.length) {
+
+            // check if the price at the start of sliding window is less than the end of sliding window
+            // means we buy on low in past and sell on high in future
+            // which is the ideal condition to maximise the profit
+            if (prices[left] < prices[right]) {
+                // calculate the profit
+                int profit = prices[right] - prices[left];
+                // assign it to final result, if the profit is higher than last result
+                res = Math.max(res, profit); 
             } else {
-                buy = sell;
+                // this is the invalid case, and we can't make any profit here
+                // so we make the length of sliding window as 1, i.e. by making left = right
+                // i.e. incrementing left to the positon of right
+                // this also means in future we are getting lower price, 
+                // so we move the left pointer to the lower price
+                left = right;
             }
-            sell += 1;
+
+            right += 1;
         }
 
-        return maxProfit;        
+        return res;        
     }
 }
