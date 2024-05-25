76e97c6376910f02535807db0ef4b3c313202af9
Swapnil Singh <swappp7singh@gmail.com>
2024-05-25 22:02:44 +0530

    Time: 6 ms (68.33%) | Memory: 44.7 MB (30.34%) - LeetSync

--- sliding-window/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ sliding-window/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,21 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> slidingWindow = new HashSet<>();
-        int right = 0;
-        int left = 0;
-        int maxLength = 0;
-        
-        while (right < s.length()){
+        int left = 0; // start of the sliding window
+        int right = 0; // end of the sliding window
+        int res = 0; // final response
+
+        while (right < s.length()) {
             if (!slidingWindow.contains(s.charAt(right))) {
                 slidingWindow.add(s.charAt(right));
-                maxLength = Math.max(maxLength, slidingWindow.size());
+                res = Math.max(res, slidingWindow.size());
                 right += 1;
             } else {
                 slidingWindow.remove(s.charAt(left));
                 left += 1;
             }
         }
-
-        return maxLength;
+        
+        return res;
     }
 }
