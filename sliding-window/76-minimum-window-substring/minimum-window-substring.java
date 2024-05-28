28b02fb45f9838d6ee2d131425cdff1d050e45d9
Swapnil Singh <swappp7singh@gmail.com>
2024-05-29 02:37:38 +0530

    Time: 2 ms (99.70%) | Memory: 44.9 MB (47.99%) - LeetSync

--- sliding-window/76-minimum-window-substring/minimum-window-substring.java
+++ sliding-window/76-minimum-window-substring/minimum-window-substring.java
@@ -1,36 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
+        // this size will store all the a-z and A-Z
+        // this map is usually much much faster than a hashmap
         int[] charFrequency = new int[128];
+        // store all the elements of t in charFrequency map
         for (char c: t.toCharArray()) {
-            charFrequency[c]++;
+            // here the int value of character, will be used as index, 
+            // which will eventually point to the same element's index
+            charFrequency[c]++; 
         }
 
-        int left = 0;
+        // sliding window boundary
+        int left = 0; 
         int right = 0;
+
+        // most important variables, see anki notes for there significance in the algorithm
         int minLeft = 0;
         int minLength = Integer.MAX_VALUE;
+
+        // required characters from target, see anki note on this for more detail
         int requiredChars = t.length();
 
+        // standard sliding window template
+        // expand the window
         while (right < s.length()) {
 
             char rightChar = s.charAt(right);
 
+            // see anki for significance of below two lines of snippets
             if (charFrequency[rightChar] > 0) requiredChars--;
             charFrequency[rightChar]--;
 
-            while (requiredChars == 0) {
+            // if required characters are matched, shrink the window
+            while (requiredChars == 0) { // this condition occurs, when all the characters of target are found in the current window
 
                 if (minLength > right - left + 1) {
-                    minLength = right - left + 1;
-                    minLeft = left;
+                    minLength = right - left + 1; // update the minimum length
+                    minLeft = left; // update the start index of the minimum window
                 }
 
                 char leftChar = s.charAt(left);
+
+                // see anki for significance of below two lines of snippets
                 charFrequency[leftChar]++;
                 if (charFrequency[leftChar] > 0) requiredChars++;
-                left++;
+
+                left++; // move the left pointer to shrink the window
             }
 
+            // standard sliding window template
             right ++;
         }
 
