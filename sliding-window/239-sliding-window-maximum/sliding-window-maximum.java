7f17c5786894eab6945302be6b482812ec5ee1ec
Swapnil Singh <swappp7singh@gmail.com>
2024-05-30 01:07:03 +0530

    Time: 33 ms (43.34%) | Memory: 65.2 MB (5.83%) - LeetSync

--- sliding-window/239-sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window/239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,23 +1,38 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        // length of the output array is (nums.length - k + 1)
+        // intuition is nums.length = k, answer is 1
+        // nums.length = 3, k = 2, answer is 2
         int[] ans = new int[nums.length - k + 1];
 
+        // pointer for ans array
         int j = 0;
+        // Monotonic decreasing queue
         Deque<Integer> q = new LinkedList<>();
         for (int i = 0; i < nums.length; ++i) {
-            if (!q.isEmpty() && q.peekFirst() < i - k + 1) q.pollFirst();
+            // if queue is not empty and index at the start of queue is less than the start of the sliding window
+            // poll from the start of the queue
+            if (!q.isEmpty() && q.peekFirst() < i - k + 1) {
+                q.pollFirst();
+            }
 
+            // if queue is not empty and incoming number is greater than the end of queue
+            // poll the end of the queue, because we are using monotonic decreasing queue
             while (!q.isEmpty() && nums[i] > nums[q.peekLast()]) {
                 q.pollLast();
             }
 
+            // insert element in queue
             q.offer(i);
 
+            // populate maximum value to ans array, as it will be present at the start of the 
+            // monotonic decreasing queue
             if (i >= k - 1) {
                 ans[j++] = nums[q.peekFirst()];
             }
         }
 
+        // return the final result
         return ans;
     }
 }
