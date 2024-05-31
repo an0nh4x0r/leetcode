1f9a37f21fb31693182660eac24f616a9621b083
Swapnil Singh <swappp7singh@gmail.com>
2024-05-31 13:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (91.78%) - LeetSync

--- sliding-window/226-invert-binary-tree/invert-binary-tree.java
+++ sliding-window/226-invert-binary-tree/invert-binary-tree.java
@@ -16,27 +16,9 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            var node = queue.poll(); // FIFO - First In First Out - Iterative Breadth First Search
-
-            // swap left and right nodes
-            var temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-
-            if (node.left != null) {
-                queue.offer(node.left);
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-        }
-
-        return root;
+        var node = new TreeNode(root.val);
+        node.left = invertTree(root.right);
+        node.right = invertTree(root.left);
+        return node;
     }
 }
