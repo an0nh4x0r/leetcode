03df463676eddd3e8db9b6074b8e1164f7f091c2
Swapnil Singh <swappp7singh@gmail.com>
2024-05-31 12:10:06 +0530

    updated

--- greedy/876-hand-of-straights/hand-of-straights.java
+++ greedy/876-hand-of-straights/hand-of-straights.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i: hand) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-
+        for (var card: hand) map.put(card, map.getOrDefault(card, 0) + 1);
+        
         Arrays.sort(hand);
 
         for (int i = 0; i < hand.length; ++i) {
-            int element = hand[i];
-            if (map.getOrDefault(element, 0) > 0) {
-                map.put(element, map.get(element) - 1);
-                for (int k = 0; k < groupSize - 1; ++k) {
-                    element++;
-                    if (map.getOrDefault(element, 0) == 0) return false;
-                    else map.put(element, map.get(element) - 1);
+            var card = hand[i];
+            if (map.getOrDefault(card, 0) > 0) {
+                for (int k = 0; k < groupSize; ++k) {
+                    if (map.getOrDefault(card + k, 0) == 0) return false;
+                    else map.put(card + k, map.getOrDefault(card + k, 0) - 1);
                 }
             }
         }
