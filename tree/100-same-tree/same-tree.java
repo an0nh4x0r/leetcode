04f70f8fc645f7b9d9b222bda0c4412c59f6aa0b
Swapnil Singh <swappp7singh@gmail.com>
2024-06-04 09:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (44.47%) - LeetSync

---
+++ tree/100-same-tree/same-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Deque<TreeNode> queue1 = new LinkedList<>();
+        Deque<TreeNode> queue2 = new LinkedList<>();
+
+        queue1.offerLast(p);
+        queue2.offerLast(q);
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            var node1 = queue1.pollFirst();
+            var node2 = queue2.pollFirst();
+
+            if (node1 == null && node2 == null) continue;
+
+            if (node1 == null || node2 == null || node1.val != node2.val) return false;
+
+            queue1.offerLast(node1.left);
+            queue2.offerLast(node2.left);
+            queue1.offerLast(node1.right);
+            queue2.offerLast(node2.right);
+        }
+
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+}
