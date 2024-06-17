b6b6f8182c31547637e2729ca00b81f32f23b9d1
Swapnil Singh <swappp7singh@gmail.com>
2024-06-17 12:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (72.04%) - LeetSync

--- tree/100-same-tree/same-tree.java
+++ tree/100-same-tree/same-tree.java
@@ -1,36 +1,46 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // This condition makes sure that we don't traverse further once both the nodes are null
-        if (p == null && q == null) {
-            return true;
+        Deque<TreeNode> stack1 = new LinkedList<>();
+        Deque<TreeNode> stack2 = new LinkedList<>();
+        boolean response = true;
+
+        stack1.push(p);
+        stack2.push(q);
+
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            TreeNode node1 = stack1.pop();
+            TreeNode node2 = stack2.pop();
+
+            if (node1 == null && node2 == null) {
+                continue;
+            }
+
+            if (node1 == null || node2 == null || node1.val != node2.val) {
+                response = false;
+                break;
+            }
+
+            stack1.push(node1.left);
+            stack2.push(node2.left);
+            stack1.push(node1.right);
+            stack2.push(node2.right);
         }
 
-        // This condition makes sure that we don't traverse further deep inside the tree, once unequal nodes
-        // are detected, i.e. once of the node is null or the values are not same in both nodes.
-        // so we would not like to traverse further in the tree and rollback from this point.
-        if (p == null || q == null || p.val != q.val) {
-            return false;
-        }
-
-        // This condition makes sure that at each node we are checking whether the left and right sub-tree are equal
-        // or not, if both the left and right subtree are equal, recursion will make sure to send true value to the
-        // parent node, otherwise it's going to return false to the parent. Once false value is sent, the parent also
-        // sends the same false value to it's parent and at the end on the recursion, we get the value as false
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return response;
     }
 }
