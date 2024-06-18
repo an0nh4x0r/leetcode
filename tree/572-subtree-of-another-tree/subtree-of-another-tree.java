f90cb2b36268760896fd2d38c19c6ccd7def2045
Swapnil Singh <swappp7singh@gmail.com>
2024-06-18 20:25:40 +0530

    Time: 2 ms (96.30%) | Memory: 44 MB (72.73%) - LeetSync

--- tree/572-subtree-of-another-tree/subtree-of-another-tree.java
+++ tree/572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,11 +15,20 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        // while recursion if we encounter null node, return false 
+        // this will make sure that we don't do any further recursion on child nodes
+        // and recursion call will go back to parent 
         if (root == null) return false;
+        // check if the given node is equal to the subRoot
         if (isSameTree(root, subRoot)) return true;
+        // we first check if the left subtree is equal to the subRoot
+        // if true we have implemented OR condition so that never gets executed
+        // if false we check the right node 
+        // if both the result are false, we send false to parent, which will now perform similar recursion
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
+    // Recursive DFS ... documentation for this method is available in the sameTree solution
     private boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
