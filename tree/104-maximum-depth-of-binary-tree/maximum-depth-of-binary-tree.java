61cea64b478f1939549d4a56e5b9ac80224a4e75
Swapnil Singh <swappp7singh@gmail.com>
2024-06-20 23:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (79.69%) - LeetSync

--- tree/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ tree/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,33 +15,9 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        // if the root is null, maxDepth is 0
         if (root == null) return 0;
-
-        // Create a double ended queue and use it as a normal queue
-        Deque<TreeNode> deque = new ArrayDeque<>();
-        deque.offerLast(root);
-
-        int maxDepth = 0;
-
-        while (!deque.isEmpty()) {
-            int dequeSize = deque.size();
-
-            for (int i = 0; i < dequeSize; ++i) {
-                TreeNode node = deque.pollFirst();
-
-                if (node.left != null) {
-                    deque.offerLast(node.left);
-                }
-
-                if (node.right != null) {
-                    deque.offerLast(node.right);
-                }
-            }
-
-            maxDepth++;
-        }
-
-        return maxDepth;
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
