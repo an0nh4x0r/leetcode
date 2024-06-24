97c1f04a8a501ab554bfb066831b0623437eebb6
Swapnil Singh <swappp7singh@gmail.com>
2024-06-25 01:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (48.61%) - LeetSync

--- tree/98-validate-binary-search-tree/validate-binary-search-tree.java
+++ tree/98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,31 +15,18 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        Deque<NodeRange> deque = new LinkedList<>();
-        deque.offerLast(new NodeRange(root, null, null));
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
-        while (!deque.isEmpty()) {
-            NodeRange nodeRange = deque.pollFirst();
-            TreeNode node = nodeRange.node();
-            Integer low = nodeRange.low();
-            Integer high = nodeRange.high();
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        // BASE CASE
+        if (root == null) return true;
 
-            if ((low != null && node.val <= low) || (high != null && node.val >= high)) {
-                return false;
-            }
-
-            if (node.left != null) {
-                deque.offerLast(new NodeRange(node.left, low, node.val));
-            }
-                        
-            if (node.right != null) {
-                deque.offerLast(new NodeRange(node.right, node.val, high));
-            }
-
+        if (max > root.val && min < root.val) {
+            return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+        } else {
+            return false;
         }
-
-        return true;
+        
     }
 }
-
-record NodeRange(TreeNode node, Integer low, Integer high) {}
