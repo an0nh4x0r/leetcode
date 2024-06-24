1a52f3c594fa3ce19b0327b9ec59010b6ac04c38
Swapnil Singh <swappp7singh@gmail.com>
2024-06-25 01:01:23 +0530

    Time: 2 ms (85.90%) | Memory: 52.5 MB (28.02%) - LeetSync

--- tree/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ tree/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -18,23 +18,25 @@ class Solution {
         return goodNodes(root, Integer.MIN_VALUE);
     }
 
-    private int goodNodes(TreeNode root, int maximum) {
-        // return 0 if the root is null
+    private int goodNodes(TreeNode root, int maxSoFar) {
+        // BASE CASE
         if (root == null) return 0;
-        
-        // node X is named good, if in the path from root to this node X
-        // there are no nodes with a value greater than X. 
-        // here maximum is the max value from the root to the current node X
-        int res = 0;
-        if (maximum <= root.val) {
-            res = 1;
-        } 
 
-        
-        res += goodNodes(root.left, Math.max(root.val, maximum)); // go left and aggregate the count of good nodes
-        res += goodNodes(root.right, Math.max(root.val, maximum)); // go right and aggregate the count of good nodes
+        // initialize goodNodesCount to 0;
+        int goodNodesCount = 0;
 
-        // finally return the result
-        return res;
+        // Check if the current node is a good node
+        if (root.val >= maxSoFar) {
+            goodNodesCount = 1;
+        }
+
+        // Update the maximum value encountered so far
+        int newMaxSoFar = Math.max(root.val, maxSoFar);
+
+        // Recursively count good nodes in the left and right subtrees
+        goodNodesCount += goodNodes(root.left, newMaxSoFar);
+        goodNodesCount += goodNodes(root.right, newMaxSoFar);
+
+        return goodNodesCount;
     }
 }
