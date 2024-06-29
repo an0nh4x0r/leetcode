ebdce4fa794d6bb3b616a3896969cf06cae78f02
Swapnil Singh <swappp7singh@gmail.com>
2024-06-30 01:14:44 +0530

    Time: 2 ms (71.87%) | Memory: 43.9 MB (79.14%) - LeetSync

--- tree/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ tree/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,28 +15,31 @@
  */
 class Solution {
 
-    int preOrderIndex = 0;
-    Map<Integer, Integer> inOrderIndexMap = new HashMap<>();
+    private int preOrderIndex = 0;
+    private Map<Integer, Integer> inOrderToIndexMap = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // populate the map with inorder values and their indices
         for (int i = 0; i < inorder.length; ++i) {
-            inOrderIndexMap.put(inorder[i], i);
+            inOrderToIndexMap.put(inorder[i], i);
         }
 
+        // build the tree using a helper function
         return buildTreeHelper(preorder, 0, inorder.length - 1);
     }
 
     private TreeNode buildTreeHelper(int[] preorder, int left, int right) {
+        // BASE CASE: if there are no elements to construct the subtree
         if (left > right) return null;
 
+        // Select the preOrderIndex element as the root and increment preOrderIndex
         int rootValue = preorder[preOrderIndex++];
         TreeNode root = new TreeNode(rootValue);
 
-        root.left = buildTreeHelper(preorder, left, inOrderIndexMap.get(rootValue) - 1);
-        root.right = buildTreeHelper(preorder, inOrderIndexMap.get(rootValue) + 1, right);
+        // Build left and right subtrees recursively
+        root.left = buildTreeHelper(preorder, left, inOrderToIndexMap.get(rootValue) - 1);
+        root.right = buildTreeHelper(preorder, inOrderToIndexMap.get(rootValue) + 1, right);
 
         return root;
     }
-
-
 }
