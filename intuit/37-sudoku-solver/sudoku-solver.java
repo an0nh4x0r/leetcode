0e3444c62ba92ea4c05b95906ddabde09179ee3d
Swapnil Singh <swappp7singh@gmail.com>
2024-07-06 11:14:33 +0530

    Time: 8 ms (57.23%) | Memory: 41 MB (32.69%) - LeetSync

--- intuit/37-sudoku-solver/sudoku-solver.java
+++ intuit/37-sudoku-solver/sudoku-solver.java
@@ -26,36 +26,34 @@ class Solution {
     }
 
     private boolean isValidPlacement(char[][] board, char num, int row, int col) {
-        return !isNumberInRow(board, num, row)
-                && !isNumberInCol(board, num, col) && !isNumberInBox(board, num, row, col);
+        return !inRow(board, num, row) && !inCol(board, num, col)
+                && !inBox(board, num, row, col);
     }
 
-    private boolean isNumberInRow(char[][] board, char num, int row) {
+    private boolean inRow(char[][] board, char num, int row) {
         for (int i = 0; i < 9; ++i) {
             if (board[row][i] == num) {
                 return true;
             }
         }
-
         return false;
     }
 
-    private boolean isNumberInCol(char[][] board, char num, int col) {
+    private boolean inCol(char[][] board, char num, int col) {
         for (int i = 0; i < 9; ++i) {
             if (board[i][col] == num) {
                 return true;
             }
         }
-
         return false;
     }
 
-    private boolean isNumberInBox(char[][] board, char num, int row, int col) {
-        int localBoxRow = row - row % 3;
-        int localBoxCol = col - col % 3;
+    private boolean inBox(char[][] board, char num, int row, int col) {
+        int localRow = row - row % 3;
+        int localCol = col - col % 3;
 
-        for (int i = localBoxRow; i < localBoxRow + 3; ++i) {
-            for (int j = localBoxCol; j < localBoxCol + 3; ++j) {
+        for (int i = localRow; i < localRow + 3; ++i) {
+            for (int j = localCol; j < localCol + 3; ++j) {
                 if (board[i][j] == num) {
                     return true;
                 }
